bf15df3a4963773bdee5a8035a1b32d50363f930
Izmailov Kirill <k2izmailov@yandex.ru>
2026-02-16 23:13:54 +0300

    page puppies

--- src/app/features/puppies/puppies.component.ts
+++ src/app/features/puppies/puppies.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { LitterHeaderComponent } from '../breeding/litter-header/litter-header.component';
+import { PuppyCardComponent } from '../breeding/puppy-card/puppy-card.component';
+import { litterHeaderByLetter } from '../breeding/data/litter-headers-by-letter';
+import { puppiesByLetter } from '../breeding/data/puppies-by-letter';
+import { littersOnPuppiesPage } from '../breeding/data/litters-on-puppies-page';
 
 @Component({
   selector: 'app-puppies',
-  imports: [RouterLink],
+  imports: [LitterHeaderComponent, PuppyCardComponent],
   templateUrl: './puppies.component.html',
   styleUrl: './puppies.component.scss'
 })
-export class PuppiesComponent {}
+export class PuppiesComponent {
+  readonly littersOnPuppiesPage = littersOnPuppiesPage;
+  readonly litterHeaderByLetter = litterHeaderByLetter;
+  readonly puppiesByLetter = puppiesByLetter;
+}
